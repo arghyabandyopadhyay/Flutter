49f3eecf306acd99ecbda98953152f9c09577e46
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2020-12-27 02:39:18 +0530

    Add files via upload

---
+++ Widgets/YoutubeAddModalBottomSheet/YoutubeAddModalOptionsModel.dart
@@ -0,0 +1,7 @@
+class YoutubeAddModalOptionsModel {
+  final String particulars;
+  final Function onTap;
+  final String icon;
+
+  YoutubeAddModalOptionsModel({this.particulars, this.onTap,this.icon});
+}
