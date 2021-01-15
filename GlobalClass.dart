7c2cb3a70eed071a1ad1adff6dad6fa299289ed5
arghyabandyopadhyay <arghyab13bga@gmail.com>
2021-01-15 18:47:59 +0530

    GlobalClass.dart

---
+++ GlobalClass.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class GlobalClass {
+}
+class CurrentTheme{
+  static const Color primaryColor = Color(0XFF[HashCodeWithoutTheHashSymbol]);
+  static const Color primaryColorLight = Color(0xFF[HashCodeWithoutTheHashSymbol]);
+  static const Color primaryText = Color(0XFF[HashCodeWithoutTheHashSymbol]);
+  static const Color secondaryText =Colors.black;
+  static const Color floatingButtonText = Colors.white;
+  static const Color floatingButtonBackground = Colors.black;
+  static const Color backgroundColorDim = Color(0xFF[HashCodeWithoutTheHashSymbol]);
+  static const Color backgroundColor = Colors.white;
+  static const Color progressBarBackground = Color(0xFF[HashCodeWithoutTheHashSymbol]);
+  static var primaryGradient=[Color(0XFF[HashCodeWithoutTheHashSymbol]),Color(0XFF[HashCodeWithoutTheHashSymbol])];
+  static var primaryGradient2=[Color(0XFF[HashCodeWithoutTheHashSymbol]),Color(0XFF[HashCodeWithoutTheHashSymbol]),Color(0XFF[HashCodeWithoutTheHashSymbol])];
+  static var buttonGradient=[Color(0XFF[HashCodeWithoutTheHashSymbol]),Color(0XFF[HashCodeWithoutTheHashSymbol])];
+}
