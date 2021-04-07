668176984c6b784e5ddb53385ef52ba6d2363522
arghyabandyopadhyay <arghyab13bga@gmail.com>
2021-04-07 18:12:55 +0530

    globalshowinsnackbar

--- Modules/WidgetHandler.dart
+++ Modules/WidgetHandler.dart
@@ -167,3 +167,17 @@ Widget buildStaggeredTile(Widget child) {
       )
   );
 }
+
+void globalShowInSnackBar(String value,SnackBarAction barAction,GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey,SnackBarBehavior barBehavior,Color barColor) {
+  if(value=="ErrorHasOccurred")value="Oops!! Error 404.";
+  else if(value=="NoData")value="No data received.";
+  else if(value=="500")value="Oops!! Error 500, It's not you, It's us.";
+  else if(value=="NoInternet")value="No Internet Connections present!!";
+  scaffoldMessengerKey.currentState.hideCurrentSnackBar();
+  scaffoldMessengerKey.currentState.showSnackBar(SnackBar(
+    behavior: barBehavior,
+    backgroundColor: barColor,
+    content: Text(value,textScaleFactor:1,style: TextStyle(),),
+    action: barAction
+  ));
+}
